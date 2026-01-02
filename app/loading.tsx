cf5d9862d6d167bf53aa0e0a68e896d96a5a7417
hyukraeyo <hyukraeyo@gmail.com>
2026-01-02 12:56:43 +0900

    style: 초기 로딩 화면에서 스피너 제거하고 로고만 표시

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,6 @@ export default function Loading() {
     return (
         <div className={styles.container}>
             <h1 className={styles.logo}>m9</h1>
-            <div className={styles.spinner}></div>
         </div>
     );
 }
