79dcc52b2e1d4592a930fb9af754e664592453d8
hyukraeyo <hyukraeyo@gmail.com>
2026-01-02 13:25:07 +0900

    style: use PageSkeleton for reservations page loading state

--- app/admin/reservations/page.tsx
+++ app/admin/reservations/page.tsx
@@ -1,12 +1,13 @@
 import { getReservations } from '../actions'
 import ReservationTable from './ReservationTable'
 import { Suspense } from 'react'
+import { PageSkeleton } from '@/app/components/Skeleton'
 
 export default async function AdminReservationsPage() {
   const reservations = await getReservations()
 
   return (
-    <Suspense fallback={<div style={{ padding: '2rem', textAlign: 'center', color: '#6b7280' }}>Loading filters...</div>}>
+    <Suspense fallback={<PageSkeleton />}>
       <ReservationTable reservations={reservations} />
     </Suspense>
   )
