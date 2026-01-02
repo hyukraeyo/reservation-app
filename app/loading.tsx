65616b3089be58fef21cb0641355ed4e4be9acff
hyukraeyo <hyukraeyo@gmail.com>
2026-01-02 12:54:50 +0900

    feat: 초기 로딩 속도 체감을 위한 스플래시 화면(로고) 추가

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,10 @@
-import LoadingPage from '@/app/components/LoadingPage';
+import styles from './loading.module.scss';
 
 export default function Loading() {
-    return <LoadingPage style={{ minHeight: '100vh' }} />;
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.logo}>m9</h1>
+            <div className={styles.spinner}></div>
+        </div>
+    );
 }
